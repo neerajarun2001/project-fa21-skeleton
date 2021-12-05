ae427271b64704701f0f1f28f7c29ba1621e593f
Michael Wu <michaelwu1002@gmail.com>
2021-12-04 19:51:25 -0800

    uncommented example solver main

--- solver.py
+++ solver.py
@@ -40,16 +40,16 @@ def solve(tasks):
 
 
 # Here's an example of how to run your solver.
-# if __name__ == '__main__':
-#     for size in os.listdir('inputs/'):
-#         if size not in ['small', 'medium', 'large']:
-#             continue
-#         for input_file in os.listdir('inputs/{}/'.format(size)):
-#             if size not in input_file:
-#                 continue
-#             input_path = 'inputs/{}/{}'.format(size, input_file)
-#             output_path = 'outputs/{}/{}.out'.format(size, input_file[:-3])
-#             print(input_path, output_path)
-#             tasks = read_input_file(input_path)
-#             output = solve(tasks)
-#             write_output_file(output_path, output)
+if __name__ == '__main__':
+    for size in os.listdir('inputs/'):
+        if size not in ['small', 'medium', 'large']:
+            continue
+        for input_file in os.listdir('inputs/{}/'.format(size)):
+            if size not in input_file:
+                continue
+            input_path = 'inputs/{}/{}'.format(size, input_file)
+            output_path = 'outputs/{}/{}.out'.format(size, input_file[:-3])
+            print(input_path, output_path)
+            tasks = read_input_file(input_path)
+            output = solve(tasks)
+            write_output_file(output_path, output)
