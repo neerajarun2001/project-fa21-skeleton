ad591bfa92d5803a5bc3abfe2e2c7d623487b2bb
Michael Wu <michaelwu1002@gmail.com>
2021-12-04 20:42:16 -0800

    first naive submission

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 from parse import read_input_file, write_output_file
 import os
 
-def solve(tasks):
+def solve_shitty_naive(tasks):
     """
     Args:
         tasks: list[Task], list of igloos to polish
@@ -10,10 +10,10 @@ def solve(tasks):
     """
     output = []
     time = 0
-    best_id = 0
-    best_pos = 0
-    list_pos = 0
-    best_profit = 0
+    best_id = -1
+    best_pos = -1
+    list_pos = -1
+    best_profit = -1
     while time < 1440:
         for task in tasks:
             benefit = task.calc_benefit_from_now(time)
@@ -29,8 +29,11 @@ def solve(tasks):
                     best_id = task.get_task_id()
                     best_pos = list_pos
             list_pos += 1
-        output.append(tasks[best_pos])
+        if len(tasks) == 0:
+            break
+        output.append(tasks[best_pos].get_task_id())
         time += tasks[best_pos].get_duration()
+        tasks.pop(best_pos)
         best_id = 0
         best_pos = 0
         list_pos = 0
@@ -51,5 +54,5 @@ if __name__ == '__main__':
             output_path = 'outputs/{}/{}.out'.format(size, input_file[:-3])
             print(input_path, output_path)
             tasks = read_input_file(input_path)
-            output = solve(tasks)
+            output = solve_shitty_naive(tasks)
             write_output_file(output_path, output)
